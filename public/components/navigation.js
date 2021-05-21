6f6b3addef361466f6c567cca739f2b438734b3b
Oliver Dixon <odix8891@uni.sydney.edu.au>
2021-05-21 12:06:09 +1000

    added setProgress to change state of progress bar

--- public/components/navigation.js
+++ public/components/navigation.js
@@ -7,12 +7,11 @@ class Navigation {
 	}
 
 	getLinks() {
-		console.log(this.links);
+		//console.log(this.links);
 	}
 
 	setPage(pageId) {
 		this.currentPage = pageId;
-		console.log(this.currentPage);
 
 		this.links.forEach((link) => {
 			link.classList.remove('active');
@@ -26,7 +25,28 @@ class Navigation {
 		})
 
 		document.getElementById(pageId).style.display = 'block';
+
+		//
+		let lis = document.getElementsByClassName('nav-links')[0].getElementsByTagName("li");
+		var isPast = false;
+		for (var i=0;i<lis.length; i++){
+			var len = lis[i].getElementsByClassName('active').length
+			var child = lis[i].children[0]
+			if (len == 1){
+				child.style.color = '#F45454';
+				child.style.fontWeight = 'bold';
+				isPast = true;
+			} else if (len == 0 && isPast == false){
+				child.style.color = '#F45454';
+				child.style.fontWeight = 'bold';
+			} else if (isPast == true){
+				child.style.color = '#D3D3D3';
+				child.style.fontWeight = 'bold';
+			}
+		}
+
 	}
+	
 
 	getHash(link){
 		return link.href.split('#')[1];
